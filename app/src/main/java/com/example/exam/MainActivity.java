727db597f54b54f68140ce70702d5e8755c35d20
Arturka585 <gartur300545@gmail.com>
2020-12-29 15:41:36 +0500

    nav_bar

--- app/src/main/java/com/example/exam/MainActivity.java
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //asdbpojfbjdfbljx 
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
